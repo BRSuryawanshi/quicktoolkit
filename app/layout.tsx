fe2662be1316b0c3e3a535d9e7a06df6fcc1752f
unknown <bhargavrajendrasuryawanshi550@gmail.com>
2026-05-31 12:53:27 +0530

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -170,6 +170,7 @@
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -277,6 +278,7 @@ export default function RootLayout({
         <div className="flex-1 w-full">
           {children}
         </div>
+        <Analytics />
       </body>
     </html>
   );
